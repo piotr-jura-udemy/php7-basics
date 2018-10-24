9c5eb12b234f4bbd088d2111e08262218e721920
Piotr Jura <>
2018-10-24 12:51:54 +0200

    001-namespaces BEFORE

---
+++ public/index.php
@@ -0,0 +1,28 @@
+<?php
+
+require __DIR__.'/../vendor/autoload.php';
+
+// use App\Format;
+// use App\Format as F;
+// use App\Format\{JSON,XML,YAML}
+// use App\Format\JSON;
+// use App\Format\XML;
+// use App\Format\YAML;
+
+// $json = new App\Format\JSON();
+// $xml = new App\Format\XML();
+// $yml = new App\Format\YAML();
+
+// $json = new F\JSON();
+// $xml = new F\XML();
+// $yml = new F\YAML();
+
+// $json = new JSON();
+// $xml = new XML();
+// $yml = new YAML();
+
+print_r("Namespaces");
+
+// print_r($json);
+// print_r($xml);
+// print_r($yml);
