0cd5f9d8bf45861f496f05920e7498fb9baa0b2a
Piotr Jura <>
2018-10-24 12:56:07 +0200

    002-class-fields-methods BEFORE

--- public/index.php
+++ public/index.php
@@ -2,27 +2,16 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
-// use App\Format;
-// use App\Format as F;
-// use App\Format\{JSON,XML,YAML}
-// use App\Format\JSON;
-// use App\Format\XML;
-// use App\Format\YAML;
+use App\Format\JSON;
+use App\Format\XML;
+use App\Format\YAML;
 
-// $json = new App\Format\JSON();
-// $xml = new App\Format\XML();
-// $yml = new App\Format\YAML();
+print_r("Class fields & methods\n\n");
 
-// $json = new F\JSON();
-// $xml = new F\XML();
-// $yml = new F\YAML();
+$json = new JSON();
+$xml = new XML();
+$yml = new YAML();
 
-// $json = new JSON();
-// $xml = new XML();
-// $yml = new YAML();
-
-print_r("Namespaces");
-
-// print_r($json);
-// print_r($xml);
-// print_r($yml);
+var_dump($json);
+var_dump($xml);
+var_dump($yml);
