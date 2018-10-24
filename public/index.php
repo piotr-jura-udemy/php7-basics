ecadc9e051c28575e1a4801dfafe79725e5eb5ab
Piotr Jura <>
2018-10-24 13:05:53 +0200

    012 BEFORE

--- public/index.php
+++ public/index.php
@@ -11,7 +11,7 @@ use App\Service\Serializer;
 use App\Controller\IndexController;
 use App\Container;
 
-print_r("Simple Service Container\n\n");
+print_r("Autowired Service Container\n\n");
 
 $container = new Container();
 
